9f3268bab79ad916998298d42ebe2df5512488a7
Marcin Stanisławek <mstanislawek@gmail.com>
2019-07-14 10:41:50 +0200

    Added User class

--- src/main/java/pl/sda/builder/App.java
+++ src/main/java/pl/sda/builder/App.java
@@ -6,8 +6,13 @@ package pl.sda.builder;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args )    {
+
+        User user = new User.Builder().name("Marcin").lastName("Stanisławek").active(true).city("Toruń").password("123456").postalCode("87-100").build();
+
+        System.out.println(user);
+
+
+
     }
 }
